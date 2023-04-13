0f3752d4e38005679ef752a470fb128fae177dfb
Alex <114528715+SightIsBindly@users.noreply.github.com>
2023-04-13 23:23:06 +0000

    Alexander Montt

--- main.py
+++ main.py
@@ -3,3 +3,9 @@
 #read in the miles and read in the gallons used.
 
 #display the miles per gallon
+A=float(input("What the number of miles?: "))
+B=float(input("What the numbers of gallons?: "))
+miles=float(A)
+Gallons=float(B)
+D=miles*Gallons
+print(D)
