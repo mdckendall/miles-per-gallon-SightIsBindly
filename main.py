cb42b6cc2d11b7868bbe2cea80f3a3a9cd57551f
Alex <114528715+SightIsBindly@users.noreply.github.com>
2023-04-13 23:38:29 +0000

    1

--- main.py
+++ main.py
@@ -4,5 +4,6 @@
 
 #display the miles per gallon
 gallon=float(input("How many gallons do you have?: "))
-miles=float(10)
-print(miles*gallon)
+miles=float(input("\nHow many miles do you have?: "))
+print("The miles per gallons were", miles/gallon)
+
